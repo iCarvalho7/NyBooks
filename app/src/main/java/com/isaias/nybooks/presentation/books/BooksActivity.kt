00db91e62ef155093df2ce3a51ec388aedd9a247
iCarvalho <isaiasd.carvalho@gmail.com>
2020-03-01 19:29:47 -0400

    App Created, and formated using MVVM standars

---
+++ app/src/main/java/com/isaias/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package com.isaias.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.isaias.nybooks.ui.activity.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
